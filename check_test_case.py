96e3266d5e552d7f365d0b84afeeb91910de0b58
jodnddus <lockhost23@gmail.com>
2021-08-22 18:22:59 +0900

    전화번호목록 풀이 코드 추가

---
+++ check_test_case.py
@@ -0,0 +1,5 @@
+def check_test_case(expect, result):
+    if expect == result:
+        print("PASS")
+    else:
+        print("ERR")
---
+++ 전화번호목록.py
@@ -0,0 +1,31 @@
+from itertools import permutations 
+
+
+def check_test_case(expect, result):
+    if expect == result:
+        print("PASS")
+    else:
+        print("ERR")
+
+
+def is_exist_head_keyword(keywords):
+    if keywords[0].startswith(keywords[1]) or keywords[1].startswith(keywords[0]):
+        return False
+    return True
+
+
+def solution(phone_book):
+    answer = True
+
+    for keywords in list(permutations(phone_book, 2)):
+        if (len(keywords[0]) <= len(keywords[1])):
+            answer = is_exist_head_keyword(keywords)
+            if not(answer):
+                break
+
+    return answer
+
+
+check_test_case(solution(["119", "97674223", "1195524421"]), False)
+check_test_case(solution(["123", "456", "789"]), True)
+check_test_case(solution(["12", "123", "1235", "567", "88"]), False)
