1883bd14f5b73dc9ecd23c13fce7b4df15fe97a0
jodnddus <lockhost23@gmail.com>
2022-01-06 13:58:23 +0900

    이중연결리스트를 이용해서 풀이

--- programmers/표편집.py
+++ programmers/표편집.py
@@ -1,29 +1,65 @@
 # https://programmers.co.kr/learn/courses/30/lessons/81303
+# 이중연결리스트
 from test import check_test_case
 
 
+class Node:
+    def __init__(self):
+        self.num = 0
+        self.removed = False
+        self.prev = None
+        self.next = None
+
+
 def solution(n, k, cmds):
-    answer = ['O' for _ in range(n)]
-    original = [n for n in range(n)]
-    table = original.copy() 
-    removes = []
+    list = [Node() for _ in range(n)]
+    stack = []
+    for i in range(1, n):
+        list[i].num = i
+        list[i-1].next = list[i]
+        list[i].prev = list[i-1]
+
+    current = list[k]
 
     for cmd in cmds:
         if cmd[0] == "U":
-            k -= int(cmd[2])
+            amount = int(cmd.split(" ")[1])
+            for _ in range(amount):
+                current = current.prev
         elif cmd[0] == "D":
-            k += int(cmd[2])
+            amount = int(cmd.split(" ")[1])
+            for _ in range(amount):
+                current = current.next
         elif cmd[0] == "C":
-            removes.append(table.copy())
-            del table[k]
+            stack.append(current.num)
+            current.removed = True
+
+            if current.prev:
+                current.prev.next = current.next
+
+            if current.next:
+                current.next.prev = current.prev
+                current = current.next
+            else:
+                current = current.prev
         elif cmd[0] == "Z":
-            old_table = removes.pop()
-            table = old_table
+            pop = stack.pop()
+            restore = list[pop]
+            restore.removed = False
 
-    for num in set([n for n in range(n)]) - set(table):
-        answer[num] = 'X'
+            if restore.prev:
+                restore.prev.next = restore
+            if restore.next:
+                restore.next.prev = restore
 
-    return "".join(answer)
+    answer = ''
+    for item in list:
+        if item.removed:
+            answer += 'X'
+        else:
+            answer += 'O'
+
+    return answer
 
 
 check_test_case(solution(
